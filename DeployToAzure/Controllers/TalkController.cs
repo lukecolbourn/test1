6dffbf26c58c2513d6d62ab8e68f3a526855bb4d
Luke <lukecolbourn@live.com>
2015-05-27 22:40:58 +0100

    Add missing controller and model.

---
+++ DeployToAzure/Controllers/TalkController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DeployToAzure.Controllers
+{
+    public class TalkController : Controller
+    {
+        //
+        // GET: /Talk/
+        public ActionResult Index()
+        {
+            return View(Messaging.MessageStorage.GetTenMessages());
+        }
+	}
+}
---
+++ DeployToAzure/Models/JumbotronModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DeployToAzure.Models
+{
+    public class JumbotronModel
+    {
+        public JumbotronModel(string title, string description)
+        {
+            this.Title = title;
+            this.Description = description;
+        }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+    }
+}
