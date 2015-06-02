34ec0eae1aa52644b709e7c0dc8086e5830d6699
Luke <lukecolbourn@live.com>
2015-06-02 22:35:16 +0100

    Set up gruntfile, minify styles and scripts.

---
+++ DeployToAzure/GruntFile.js
@@ -0,0 +1,38 @@
+﻿module.exports = function (grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        files: [{
+          src: 'scripts/*/*.js',
+          dest: 'scripts/app.min.js'
+        }]
+      }
+    },
+    cssmin: {
+      options: {
+        shorthandCompacting: false,
+        roundingPrecision: -1
+      },
+      target: {
+        files: {
+          'Content/Messaging/Messaging.min.css': 'Content/Messaging/*.css'
+          }
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('uglifycss', ['cssmin']);
+  grunt.registerTask('min', ['cssmin', 'uglify']);
+};
